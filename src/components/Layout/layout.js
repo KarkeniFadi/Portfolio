d3e9d9d8585f57fc9aef6dbc6ece5dc51aa7672c
Karkeni Fadi <karkeni.fadi@gmail.com>
2022-09-15 23:47:15 +0100

    layout:Sidebar

---
+++ src/components/Layout/layout.js
@@ -0,0 +1,12 @@
+import Sidebar from '../Sidebar/sidebar';
+import './layout.scss';
+
+const Layout = () => {
+    return (
+        <div className=''>
+            <Sidebar />
+        </div>
+    )
+
+}
+export default Layout;
