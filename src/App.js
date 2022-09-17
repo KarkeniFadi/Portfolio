96fa2e85dd73bb7f1f98654574b1bd665e03a7e7
Karkeni Fadi <karkeni.fadi@gmail.com>
2022-09-17 09:50:34 +0100

    Home:About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Routes , Route } from 'react-router-dom';
+import Layout from './components/Layout/layout';
+import Home from './components/Home/home';
+import About from './components/About/about';
+import Contact from './components/Contact/contact';
 import './App.scss';
-import { Routes,Route } from 'react-router-dom';
-import Layout from './components/Layout/layout'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route  path="/" element={<Layout />}  />
+        <Route  path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Route>
       </Routes>
     </>
   );
--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,10 +1,10 @@
 import './sidebar.scss';
-import LogoF from '../../assets/images/logoo.png'
-import LogoSub from '../../assets/images/logo_sub.png'
+import LogoF from '../../assets/images/logo-f1.png'
+import LogoSub from '../../assets/images/logo_sub1.png'
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin,  } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faInstagram, faLinkedin,  } from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => (
@@ -19,11 +19,11 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+            <NavLink  activeclassname="active" className="about-link" to="/about" >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
+            <NavLink  activeclassname="active" className="contact-link" to="/contact" >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
@@ -37,6 +37,11 @@ const Sidebar = () => (
             <li>
                 <a target="_blank" rel="noreferrer" href='https://github.com/KarkeniFadi'>
                     <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href='https://www.instagram.com/karkenifedi/'>
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
                 </a>
             </li>
            
